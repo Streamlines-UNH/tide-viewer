9c647261926edcc2e4c0701187166afc8f7fcf88
Jack Hamilton <jth1024@wildcats.unh.edu>
2019-11-22 13:46:41 -0500

    "sass and react file structure and stubbing"

---
+++ src/components/Map.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Map(props) {
+    return (
+        <p>Map test</p>
+    );
+}
+
+export default Map;
---
+++ src/components/MapControls.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function MapControls(props) {
+    return (
+        <p>MapControls test</p>
+    );
+}
+
+export default MapControls;
