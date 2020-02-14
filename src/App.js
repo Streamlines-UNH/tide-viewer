73290d16fb1b91e1951432a854f198d947cc67b2
Jack Hamilton <jack.hamilton207@gmail.com>
2020-02-14 14:03:06 -0500

    clode cleanup

--- src/App.js
+++ src/App.js
@@ -3,17 +3,13 @@ import './App.css';
 
 import CurrentStrength from './components/CurrentStrength';
 import Map from './components/Map';
-import ThemeSelector from './components/ThemeSelector';
 
 function App() {
-  const center = [0,0];
-  //const [zoom, setZoom] = useState(1);
 
   return (
     <div className="App">
       <CurrentStrength />
-      <Map center={center} zoom={1} /> 
-      <ThemeSelector />
+      <Map/> 
     </div>
   );
 }
