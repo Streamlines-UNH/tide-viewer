218dec113edeac17c5c1ff164f5c274934a7c9b6
Matthew Hartman <msh1011@wildcats.unh.edu>
2020-02-24 12:13:51 -0500

    added comments

--- src/components/Map.js
+++ src/components/Map.js
@@ -8,7 +8,8 @@ function CurrentMap() {
 	const mapRef = useRef();
 	useEffect(
 		() => {
-			// lazy load the required ArcGIS API for JavaScript modules and CSS
+			// lazy load the required ArcGIS API for JavaScript modules
+			// This is how arcgis is build to be imported, (babel)
 			loadModules([
 				"esri/Map",
 				"esri/core/watchUtils",
@@ -25,6 +26,7 @@ function CurrentMap() {
 					BaseLayerViewGL2D,
 					OpenStreetMapLayer]) => {
 
+						// This will init the WebGL graphics
 						var CustomLayerView2D = BaseLayerViewGL2D.createSubclass(StreamlinesGL(watchUtils));
 
 						// Subclass the layer view from GraphicsLayer, to take advantage of its
@@ -43,11 +45,14 @@ function CurrentMap() {
 						var map = new Map();
 
 						var openLayer = new OpenStreetMapLayer()
+						// copy the function that is called when a tile enters view
 						openLayer.realFetchTile = openLayer.fetchTile
+						// save the tile data at each zxy
 						var zxyGraphics = {}
 						var graphicsTimer;
 						map.layers.add(openLayer);
 
+						// Define the mapview
 						var view = new MapView({
 							container: "viewDiv",
 							map: map,
@@ -55,8 +60,29 @@ function CurrentMap() {
 							zoom: 9
 						});
 
-						var layer = new CustomLayer({});
-						map.layers.add(layer);
+						var Streamlineslayer = new CustomLayer({});
+						map.layers.add(Streamlineslayer);
+
+						// callback function that will reset the layer on zoom/pan
+						function redraw_geometry(z) {
+							return function(){
+								var g = []
+								for (const key of Object.keys(zxyGraphics)) {
+									if (key.startsWith(`${z}`)){
+										for (var i = 0; i < zxyGraphics[key].length; i++){
+											g.push(zxyGraphics[key][i])
+										}
+									}
+								}
+								map.layers.remove(Streamlineslayer)
+								Streamlineslayer = new CustomLayer({
+									graphics: g
+								});
+								map.layers.add(Streamlineslayer)
+							}
+						}
+
+						// get a gradient based on a 0-1 float between color1 and color2
 						var color1 = [255, 0, 0]
 						var color2 = [0, 255, 0]
 						function color_from_mag(mag) {
@@ -67,6 +93,9 @@ function CurrentMap() {
 								Math.round(color1[2] * w1 + color2[2] * w2)];
 							return rgb;
 						}
+						// arcgis will call this function when a tile comes into view
+						// so we can hijack that call and get the tiles from our API
+						// then return the saved function from earlier
 						openLayer.fetchTile = function(z, r, c, op) {
 							var url = `https://p648saeyvc.execute-api.us-east-1.amazonaws.com/Prod/api/CBOFS/9/${z}/${c}/${r}`
 							//var url = `http://localhost:8000/services/CBOFS/tiles/${z}/${c}/${r}`
@@ -77,10 +106,13 @@ function CurrentMap() {
 								y: r
 							}, function (err, result) {
 								if (err) return;
-								var t = result.features.map(function(trip) {
+								// modify the geojson to something the renderer can use
+								zxyGraphics[`${z}/${c}/${r}`] = result.features.map(function(trip) {
+									// remap the magnitudes to a color value between color1 and color2
 									var mags = trip.properties.magnitudes.substring(1, trip.properties.length)
 										.split(',').map(parseFloat)
 										.map(color_from_mag)
+									// Redefine the lat/long to EPSG:4326 for use in the renderer
 									return {
 										attributes: {
 											color: mags
@@ -94,31 +126,17 @@ function CurrentMap() {
 										})
 									};
 								});
-								zxyGraphics[`${z}/${c}/${r}`] = t
+
+								// after we are done fetching new tiles recompute the graphics layer
 								clearTimeout(graphicsTimer)
-								graphicsTimer = setTimeout(function() {
-									var g = []
-									const keys = Object.keys(zxyGraphics)
-									for (const key of keys) {
-										if (key.startsWith(`${z}`)){
-											for (var i = 0; i < zxyGraphics[key].length; i++){
-												g.push(zxyGraphics[key][i])
-											}
-										}
-									}
-									map.layers.remove(layer)
-									layer = new CustomLayer({
-										graphics: g
-									});
-									map.layers.add(layer)
-								}, 200)
+								graphicsTimer = setTimeout(redraw_geometry(z), 200)
 							});
 							return openLayer.realFetchTile(z, r, c, op)
 						}
 
+						// return a deconstructor
 						return () => {
 							if (view) {
-								// destroy the map view
 								view.container = null;
 							}
 						};
